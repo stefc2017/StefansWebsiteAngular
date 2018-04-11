0717fb3258f62f5f8ff9769a65e3088aed028915
Stefan <stefc2013@gmail.com>
2018-04-11 17:58:15 -0500

    2018-04-11: Added Bootstrap to angular project

---
+++ StefansWebsiteAngular/src/app/business/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: '../presentation/html/navbar.component.html',
+  styleUrls: ['../presentation/css/navbar.component.css']
+})
+export class NavbarComponent {
+}
