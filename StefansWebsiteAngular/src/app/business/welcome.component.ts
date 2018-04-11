ad2851de38de8d8d6629785a2ade06f50b076c7b
Stefan <stefc2013@gmail.com>
2018-04-11 18:51:40 -0500

    2018-04-11 - Added welcome page

---
+++ StefansWebsiteAngular/src/app/business/welcome.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: '../presentation/html/welcome.component.html',
+  styleUrls: ['../presentation/css/welcome.component.css']
+})
+export class WelcomeComponent {
+}
