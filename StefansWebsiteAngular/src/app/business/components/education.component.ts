463253669dd0a2c7cec822e29018c628e66eb9c2
Stefan <stefc2013@gmail.com>
2018-04-22 08:53:43 -0500

    2018-04-22 Added Education Section

--- StefansWebsiteAngular/src/app/business/components/education.component.ts
+++ StefansWebsiteAngular/src/app/business/components/education.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { EducationService } from '../services/education.service';
+import { Education } from '../../data_model/education';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: '../../presentation/html/app.component.html',
-  styleUrls: ['../../presentation/css/app.component.css']
+  selector: 'app-education',
+  templateUrl: '../../presentation/html/education.component.html',
+  styleUrls: ['../../presentation/css/education.component.css']
 })
-export class AppComponent {
+export class EducationComponent {
+  myEducation: Education[];
+  errMessage: string;
+
+  constructor(private educationService: EducationService){}
+
+  ngOnInit(){
+    this.getAllMyEducation();
+  }
+
+  getAllMyEducation(){
+    this.educationService.getAllMyEducation()
+    .subscribe(
+      res => this.myEducation = res,
+      err => this.errMessage = err
+    );
+  }//end getAllMyEducation
+
+  
 }
--- StefansWebsiteAngular/src/app/shared/app.module.ts
+++ StefansWebsiteAngular/src/app/shared/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { HttpHandler } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { VerticalTimelineModule } from 'angular-vertical-timeline';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from '../business/components/app.component';
 import { NavbarComponent } from '../business/components/navbar.component';
 import { WelcomeComponent } from '../business/components/welcome.component';
 import { FooterComponent } from '../business/components/footer.component';
+import { EducationComponent } from '../business/components/education.component';
 import { EducationService } from '../business/services/education.service';
 
 @NgModule({
@@ -14,13 +20,18 @@ import { EducationService } from '../business/services/education.service';
     AppComponent,
     NavbarComponent,
     WelcomeComponent,
-    FooterComponent
+    FooterComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    HttpClientModule,
+    VerticalTimelineModule
   ],
   providers: [EducationService],
-  bootstrap: [AppComponent, NavbarComponent, WelcomeComponent, FooterComponent]
+  bootstrap: [AppComponent, NavbarComponent, WelcomeComponent, FooterComponent, EducationComponent]
 })
 export class AppModule { }
